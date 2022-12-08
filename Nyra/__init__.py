7212f2656878f1a3d8e77204524002fad2f6167c
Mohit <102339239+Wolf2901@users.noreply.github.com>
2022-12-08 19:43:57 +0530

    Add files via upload

---
+++ Nyra/__init__.py
@@ -0,0 +1,41 @@
+from Nyra.core.bot import NyraBot
+from Nyra.core.dir import dirr
+from Nyra.core.git import git
+from Nyra.core.userbot import Userbot
+from Nyra.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = NyraBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
